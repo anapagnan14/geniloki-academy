316717ccc5a6d9d0eb782f993213be6cba634074
anapagnan14 <pagnanana14@gmail.com>
2026-05-09 20:58:06 -0300

    connect lessons route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import gameRoutes from "./routes/games.js";
+import lessonRoutes from "./routes/lessons.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/games", gameRoutes);
+app.use("/api/lessons", lessonRoutes);
 
 app.get("/", (req, res) => {
   res.send("API rodando 🚀");
