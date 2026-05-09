9f9eff706b1966027d99f237c7bc0885a9912997
anapagnan14 <pagnanana14@gmail.com>
2026-05-09 20:30:14 -0300

    corrige games route

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,21 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
 
-import gameRoutes from "./routes/games.js";
-import lessonRoutes from "./routes/lessons.js";
-import aiRoutes from "./routes/ai.js";
-
-dotenv.config();
+import gameRoutes from "./routes/Games.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/geniloki");
-
 app.use("/api/games", gameRoutes);
-app.use("/api/lessons", lessonRoutes);
-app.use("/api/ai", aiRoutes);
 
-app.listen(3000, () => console.log("🚀 API rodando"));
+app.get("/", (req, res) => {
+  res.send("API rodando 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🚀 API rodando");
+});
