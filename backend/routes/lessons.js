63032f40c2595580a4d5309e1951a1b574aa5232
anapagnan14 <pagnanana14@gmail.com>
2026-05-13 15:36:46 -0300

    fix lessons api

--- backend/routes/lessons.js
+++ backend/routes/lessons.js
@@ -1,13 +1,26 @@
 import express from "express";
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const router = express.Router();
 
-const filePath = "./data/lessons.json";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(
+  __dirname,
+  "../data/lessons.json"
+);
+
 // PEGAR AULAS
 router.get("/", (req, res) => {
 
   try {
+
+    if (!fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, "[]");
+    }
 
     const data = fs.readFileSync(
       filePath,
@@ -32,6 +45,10 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
 
   try {
+
+    if (!fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, "[]");
+    }
 
     const lessons = JSON.parse(
       fs.readFileSync(filePath, "utf-8")
